11df02979a96e4923bdec93646d035f75ae979a8
Ksaper <ofek4430@gmail.com>
2021-08-21 01:58:55 +0300

    Use custom menu module

--- menu.lua
+++ menu.lua
@@ -13,7 +13,7 @@
 --]]
 
 local Gio        = require("lgi").Gio
-local awful_menu = require("awful.menu")
+local awful_menu = require("modules.menu")
 local menu_gen   = require("menubar.menu_gen")
 local menu_utils = require("menubar.utils")
 
